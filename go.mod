62b8a4320a63c26ced6a1c24964fcb9b0cc567be
Roman Timoshkin <rmntim@yandex.ru>
2025-01-27 11:59:08 +0300

    :notebook: feat: generate id for bins

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rmntim/xbin
 
 go 1.23.5
 
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
--- internal/repo/bins/sqlite/sqlite.go
+++ internal/repo/bins/sqlite/sqlite.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log/slog"
 
+	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 	repo "github.com/rmntim/xbin/internal/repo/bins"
 	svcErr "github.com/rmntim/xbin/internal/services/bins/errors"
@@ -48,14 +49,19 @@ func (r *repository) Get(ctx context.Context, id string) (models.Bin, error) {
 }
 
 func (r *repository) Create(ctx context.Context, newBin models.NewBin) (models.Bin, error) {
-	stmt, err := r.db.PrepareContext(ctx, "INSERT INTO bins (content) VALUES (?) RETURNING id, content, created_at")
+	stmt, err := r.db.PrepareContext(ctx, "INSERT INTO bins (id, content) VALUES (?, ?) RETURNING id, content, created_at")
 	if err != nil {
 		return models.Bin{}, fmt.Errorf("could not prepare statement: %w", err)
 	}
 	defer stmt.Close()
 
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return models.Bin{}, fmt.Errorf("error creating uuid: %w", err)
+	}
+
 	var bin models.Bin
-	err = stmt.QueryRowContext(ctx, newBin.Content).Scan(&bin.Id, &bin.Content, &bin.CreatedAt)
+	err = stmt.QueryRowContext(ctx, id, newBin.Content).Scan(&bin.Id, &bin.Content, &bin.CreatedAt)
 	if err != nil {
 		return models.Bin{}, fmt.Errorf("could not create bin: %w", err)
 	}
